a0760dede812b1a8481d792200f8efe60d3d85cb
Rakesh Kumar S <rakesh.kumar@aspirenxt.com>
2021-04-19 19:18:37 +0530

    second commit

--- VPC.tf
+++ VPC.tf
@@ -1,16 +1,16 @@
-resource "aws_vpc" "prod-vpc" {
+resource "aws_vpc" "prod" {
     cidr_block = "100.0.0.0/16"
     instance_tenancy = "default"
     
     tags = {
-        Name = "prod-vpc"
+        Name = "prod"
 
     }
   
 }
 
-resource "aws_internet_gateway" "gw" {
-    vpc_id = aws_vpc.prod-vpc.id
+resource "aws_internet_gateway" "prod-gw" {
+    vpc_id = aws_vpc.prod.id
 
     tags = {
         Name = "myigw"
@@ -18,7 +18,7 @@ resource "aws_internet_gateway" "gw" {
 }
 
 resource "aws_subnet" "public-subnet" {
-    vpc_id = aws_vpc.prod-vpc.id
+    vpc_id = aws_vpc.prod.id
     cidr_block = "100.0.1.0/24"
     availability_zone = "ap-southeast-1a"
 
@@ -27,3 +27,14 @@ resource "aws_subnet" "public-subnet" {
     }
 }
 
+resource "aws_route_table" "Pub-Prod" {
+  vpc_id = aws_vpc.prod.id
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.prod-gw.id
+  }
+
+  tags = {
+    Name = "Pub-Prod"
+  }
+}
