b6a12541324a180766314c0e804105087695aff2
Rakesh Kumar S <rakesh.kumar@aspirenxt.com>
2021-04-23 19:07:08 +0530

    modified changes in nginx server

--- docinstance.tf
+++ docinstance.tf
@@ -2,12 +2,12 @@ resource "aws_instance" "terraform" {
     ami = "ami-03ca998611da0fe12"
     instance_type = "t2.micro"
     subnet_id = aws_subnet.public-subnet.id
-    vpc_security_group_ids =[aws_security_group.prod-sg.id]
+    vpc_security_group_ids =[aws_security_group.production-sg.id]
 
     associate_public_ip_address = true
     key_name = "iphone"
 
-    user_data = file("docker.sh")
+    user_data = file("doc.sh")
 
     tags = {
         Name = "terraform"
